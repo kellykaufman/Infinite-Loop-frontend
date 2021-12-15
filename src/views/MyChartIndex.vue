666ea2e843dd80be46b4fff0d41cec9dd14d65a2
Kelly Kaufman <kellykaufman12@gmail.com>
2021-12-15 10:55:09 -0800

    adds this. and axios.get

--- src/views/MyChartIndex.vue
+++ src/views/MyChartIndex.vue
@@ -7,13 +7,24 @@
 <style></style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
       anxieties: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.anxietiesindex();
+  },
+  methods: {
+    anxietiesindex: function () {
+      axios.get("/anxieties").then((response) => {
+        this.anxieties = response.data;
+        console.log("All anxieties", this.anxieties);
+      });
+    },
+  },
 };
 </script>
