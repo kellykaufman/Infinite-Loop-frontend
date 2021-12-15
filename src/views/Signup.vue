cf0df852f13e054143329e930413fccc2ff3a8f7
Kelly Kaufman <kellykaufman12@gmail.com>
2021-12-15 11:33:09 -0800

    got signup to work

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -6,8 +6,8 @@
         <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
       </ul>
       <div>
-        <label>Name:</label>
-        <input type="text" v-model="newUserParams.name" />
+        <label>Full Name:</label>
+        <input type="text" v-model="newUserParams.full_name" />
       </div>
       <div>
         <label>Email:</label>
