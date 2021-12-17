3db3b0a3137c295619eab77f0aad0d3f05d5e04c
Kelly Kaufman <kellykaufman12@gmail.com>
2021-12-17 11:11:25 -0800

    Makes login signup invisible when user is logged in

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/chart-index">My Chart</router-link>
-      |
-      <router-link to="/signup">Signup</router-link>
-      |
-      <router-link to="/login">Login</router-link>
-      |
-      <router-link to="/logout">Logout</router-link>
+      <router-link v-if="isLoggedIn() === true" to="/chart-index"
+        >My Chart |</router-link
+      >
+
+      <router-link v-if="isLoggedIn() === false" to="/signup"
+        >Signup
+      </router-link>
+
+      <router-link v-if="isLoggedIn() === false" to="/login"
+        >| Login</router-link
+      >
+
+      <router-link v-if="isLoggedIn() === true" to="/logout"
+        >Logout</router-link
+      >
     </div>
     <router-view />
   </div>
@@ -35,3 +43,25 @@
   color: #42b983;
 }
 </style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      flashMessage: "",
+    };
+  },
+  methods: {
+    isLoggedIn: function () {
+      if (localStorage.getItem("jwt")) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    getUserId: function () {
+      return localStorage.getItem("user_id");
+    },
+  },
+};
+</script>
