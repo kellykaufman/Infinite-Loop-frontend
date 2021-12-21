d91f76a54094c3a4dcf9940e8166140da13add16
Kelly Kaufman <kellykaufman12@gmail.com>
2021-12-21 12:03:37 -0800

    Formats anxiety themes list

--- src/views/AnxietyThemes.vue
+++ src/views/AnxietyThemes.vue
@@ -1,35 +1,126 @@
 <template>
   <div class="anxiety-themes">
     <div class="container">
-      1. All-or-nothing thinking – Viewing a situation in only two categories
-      instead of on a continuum. EXAMPLE: “Everybody has to like me.” 2.
-      Catastrophizing – Making a sweeping negative conclusion, without
-      considering other more likely possibilities. EXAMPLE: “If I approach that
-      woman, she will reject me, everybody will see it, and they will all think
-      I am a total loser.” 3. Magnifying – Unreasonably magnifying the
-      importance of something. EXAMPLE: “If I speak up in class and stumble over
-      my words, it will be horrible.” 4. Discounting or minimizing the positive
-      – Unreasonably telling your self that positive experiences or qualities do
-      not count. EXAMPLE: “I was able to give that work presentation last
-      week…but there is no way I can do that today!” 5. Emotional reasoning –
-      Thinking something must be true because you “feel” it so strongly.
-      EXAMPLE: I “feel” like I am stupid when I speak publicly, so I am stupid.”
-      6. Labeling – Putting a fixed, global label on your self without
-      considering evidence that might lead to a less disastrous conclusion.
-      EXAMPLE: “I am a social basket case – a “freak”.” 7. Selective Abstraction
-      / Tunnel Vision – Over-attending to one negative detail instead of seeing
-      the whole picture. EXAMPLE: “I stumbled over my words on this job
-      interview – they will never hire me now.” 8. “Should” and “must”
-      statements (perfectionism) – Having precise, fixed, unchangeable ideas of
-      how things should be. EXAMPLE: “I should never feel any anxiety in social
-      situations.” 9. Comparison – Comparing your experience to others’ and
-      assuming you have it worse than them. EXAMPLE: “My friend doesn’t have
-      social anxiety - her life is perfect!” 10. Mind reading – Believing you
-      know what others are thinking, without considering other possibilities.
-      EXAMPLE: “Everybody is looking at me, and they all think I am a dork.” 11.
-      Personalization – Believing others are behaving in a certain way because
-      of you, without considering other possibilities. EXAMPLE: “Those people I
-      met at the party left early because they couldn’t stand me.”
+      <p>
+        <b> 1. All-or-nothing thinking: </b>
+        <span>
+          Viewing a situation in only two categories instead of on a continuum.
+          <p><b>EXAMPLE:</b> “Everybody has to like me.”</p>
+        </span>
+      </p>
+      <p>
+        <b> 2. Catastrophizing: </b>
+        <span>
+          Making a sweeping negative conclusion, without considering other more
+          likely possibilities.
+          <p>
+            <b>EXAMPLE:</b> “If I approach that woman, she will reject me,
+            everybody will see it, and they will all think I am a total loser.”
+          </p>
+        </span>
+      </p>
+      <p>
+        <b> 3. Magnifying: </b>
+        <span>
+          Unreasonably magnifying the importance of something.
+
+          <p>
+            <b>EXAMPLE:</b> “If I speak up in class and stumble over my words,
+            it will be horrible.”
+          </p>
+        </span>
+      </p>
+      <p>
+        <b> 4. Discounting or minimizing the positive: </b>
+        <span>
+          Unreasonably telling your self that positive experiences or qualities
+          do not count.
+
+          <p>
+            <b>EXAMPLE:</b> “I was able to give that work presentation last
+            week…but there is no way I can do that today!”
+          </p>
+        </span>
+      </p>
+      <p>
+        <b> 5. Emotional Reasoning: </b>
+        <span>
+          Thinking something must be true because you “feel” it so strongly.
+
+          <p>
+            <b>EXAMPLE:</b> “ I “feel” like I am stupid when I speak publicly,
+            so I am stupid.”
+          </p>
+        </span>
+      </p>
+      <p>
+        <b> 6. Labeling: </b>
+        <span>
+          Putting a fixed, global label on your self without considering
+          evidence that might lead to a less disastrous conclusion.
+
+          <p><b>EXAMPLE:</b> “I am a social basket case – a “freak.”</p>
+        </span>
+      </p>
+      <p>
+        <b> 7. Selective Abstraction / Tunnel Vision: </b>
+        <span>
+          Over-attending to one negative detail instead of seeing the whole
+          picture.
+
+          <p>
+            <b>EXAMPLE:</b> “I stumbled over my words on this job interview –
+            they will never hire me now.”
+          </p>
+        </span>
+      </p>
+      <p>
+        <b> 8. "Should" and "Must" statements (perfectionism): </b>
+        <span>
+          Having precise, fixed, unchangeable ideas of how things should be.
+
+          <p>
+            <b>EXAMPLE:</b> ““I should never feel any anxiety in social
+            situations.”
+          </p>
+        </span>
+      </p>
+      <p>
+        <b> 9. Comparison: </b>
+        <span>
+          Comparing your experience to others’ and assuming you have it worse
+          than them.”
+
+          <p>
+            <b>EXAMPLE:</b> “My friend doesn’t have social anxiety - her life is
+            perfect!”
+          </p>
+        </span>
+      </p>
+      <p>
+        <b> 10. Mind reading: </b>
+        <span>
+          Believing you know what others are thinking, without considering other
+          possibilities.
+
+          <p>
+            <b>EXAMPLE:</b> “Everybody is looking at me, and they all think I am
+            a dork.”
+          </p>
+        </span>
+      </p>
+      <p>
+        <b> 11. Personalization: </b>
+        <span>
+          Believing others are behaving in a certain way because of you, without
+          considering other possibilities.
+
+          <p>
+            <b>EXAMPLE:</b> “Those people I met at the party left early because
+            they couldn’t stand me.”
+          </p>
+        </span>
+      </p>
     </div>
   </div>
 </template>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -5,49 +5,79 @@
       style="background-image: linear-gradient(to left, #7c24ff, #0b53ff)"
     >
       <div class="container">
-        <div class="row mt-6 align-items-center">
-          <div class="col-md-6 text-white mt-5">
-            <h2 class="rtx-bold display-md-1 text-white">INFINITE LOOP</h2>
-            <p class="lead text-white-08 my-4">
-              An anxiety app to help users track their anxiety.
-            </p>
-            <a
-              class="btn btn-warning-gradient btn-pill width-10rem m-2"
-              href="/signup"
-              >Signup</a
-            ><a class="btn btn-white btn-pill width-10rem m-2" href="/login"
-              >Login</a
-            >
+        <div class="row mt-5 align-items-center">
+          <div class="col-md-8 text-white mt-6">
+            <div class="center-pretty">
+              <img class="image" src="../assets/signup-IMG.png" alt="image" />
+              <p class="lead text-white-08 my-4">
+                An app to help users track their anxiety.
+              </p>
+              <form v-on:submit.prevent="submit()">
+                <ul>
+                  <li v-for="error in errors" v-bind:key="error">
+                    {{ error }}
+                  </li>
+                </ul>
+                <div class="please-work">
+                  <label>Full Name: </label>
+                  <input type="text" v-model="newUserParams.full_name" />
+                </div>
+                <p></p>
+                <div class="please-work">
+                  <label>Email: </label>
+                  <input type="email" v-model="newUserParams.email" />
+                </div>
+                <p></p>
+                <div class="please-work">
+                  <label>Password: </label>
+                  <input type="password" v-model="newUserParams.password" />
+                </div>
+                <p></p>
+                <div class="please-work">
+                  <label>Password confirmation: </label>
+                  <input
+                    type="password"
+                    v-model="newUserParams.password_confirmation"
+                  />
+                </div>
+
+                <p></p>
+                <br />
+                <input
+                  class="btn btn-warning-gradient btn-pill width-10rem m-2"
+                  type="submit"
+                  value="Signup"
+                />
+              </form>
+            </div>
           </div>
           <div class="col-md-6 text-right"></div>
         </div>
       </div>
     </section>
-    <form v-on:submit.prevent="submit()">
-      <h1>Signup</h1>
-      <ul>
-        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-      </ul>
-      <div>
-        <label>Full Name:</label>
-        <input type="text" v-model="newUserParams.full_name" />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input type="email" v-model="newUserParams.email" />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" v-model="newUserParams.password" />
-      </div>
-      <div>
-        <label>Password confirmation:</label>
-        <input type="password" v-model="newUserParams.password_confirmation" />
-      </div>
-      <input type="submit" value="Submit" />
-    </form>
   </div>
 </template>
+
+<style>
+.signup {
+  text-align: center;
+}
+
+.center-pretty {
+  text-align: center;
+  margin-left: 350px;
+}
+
+.please-work {
+  text-align: left;
+  margin-left: 20px;
+}
+
+.image {
+  height: 150px;
+  width: 150px;
+}
+</style>
 
 <script>
 import axios from "axios";
