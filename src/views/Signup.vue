f42737899d2eb503e084b67d97dc7ea25e893223
Kelly Kaufman <kellykaufman12@gmail.com>
2022-01-28 17:27:23 -0800

    Puts path back in signup file

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -106,7 +106,7 @@ export default {
         .post("/users", this.newUserParams)
         .then((response) => {
           console.log(response.data);
-          this.$router.push("/login");
+          this.$router.push("/path");
         })
         .catch((error) => {
           this.errors = error.response.data.errors;
